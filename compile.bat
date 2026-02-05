665b1359434a4c425fc636ecd8729ae79f36e09e
Daviddedic2008 <david.dedic.2008@gmail.com>
2026-02-05 08:51:16 -0500

    add batch

---
+++ compile.bat
@@ -0,0 +1,23 @@
+@echo off
+SETLOCAL EnableDelayedExpansion
+
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do set ESC=%%b
+set GREEN=!ESC![92m
+set RED=!ESC![91m
+set YELLOW=!ESC![93m
+set BLUE=!ESC![96m
+set RESET=!ESC![0m
+
+set "inputFile=%~1"
+
+if exist "build\CortexM4Compiler.exe" (
+    echo.
+    echo %GREEN%[SUCCESS] Build found.%RESET%
+    echo %YELLOW%--- Running ---%RESET%
+    echo.
+    build\CortexM4Compiler.exe %inputFile%
+) else (
+    echo.
+    echo %RED%[ERROR] Build failed. Check the errors above.%RESET%
+    pause
+)
